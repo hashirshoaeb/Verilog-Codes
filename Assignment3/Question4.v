c0f5b2203b517732af587f44833b8ca4245844b4
hashirshoaeb <hashirshoaeb@gmail.com>
2019-11-01 02:15:41 +0500

    question4

--- Assignment3/Question4.v
+++ Assignment3/Question4.v
@@ -1,27 +1,27 @@
 // Q2: Exercise question 2.10. Provide an RTL diagram and its relevant code; also test your code using a stimulus. Keep the input and output bit size equal to 4 bits.
 // y[n] = x[n] - x[n-1] + x[n-2] +x[n-3] +0.5y[n-1] +0.25y[n-2]
 
-module bcd(bcd_num,
-           out);
+module Question4(num,
+                 out);
     
-    input[3:0] bcd_num;
+    input[3:0] num;
     output[6:0] out;
     reg[6:0] out;
     
-    always@(bcd_num)
+    always@(num)
     begin
         case(bcd_num)
-            0:out       = 7'b0000001;
-            1:out       = 7'b1001111;
-            2:out       = 7'b0010010;
-            3:out       = 7'b0000110;
-            4:out       = 7'b1001100;
-            5:out       = 7'b0100100;
-            6:out       = 7'b0100000;
-            7:out       = 7'b0001111;
-            8:out       = 7'b0000000;
-            9:out       = 7'b0001100;
-            default:out = 7'b1111111;
+            0:out       = 1;   // 0000001
+            1:out       = 79;  // 1001111
+            2:out       = 18;  // 0010010
+            3:out       = 6;   // 0000110
+            4:out       = 76;  // 1001100
+            5:out       = 36;  // 0100100
+            6:out       = 32;  // 0100000
+            7:out       = 15;  // 0001111
+            8:out       = 0;   // 0000000
+            9:out       = 12;  // 0001100
+            default:out = 127; // 1111111
         endcase
     end
 endmodule
