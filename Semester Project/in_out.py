11f3302a6c4fe959d557dbed2d09ef26b0c14013
hashirshoaeb <hashirshoaeb@gmail.com>
2019-12-06 13:29:27 +0500

    2d to linear in txt

---
+++ Semester Project/in_out.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+image2d = cv.imread('image.png', flags=cv.IMREAD_GRAYSCALE)
+# print(image2d)
+
+# Write and Read (‘w+’) :
+#           Open the file for reading and writing.
+#           For existing file, data is truncated and over-written.
+#           The handle is positioned at the beginning of the file.
+
+f = open("in.txt","w+")
+for rows in image2d:
+    for pixel in rows:
+        # print(j)
+        f.writelines([str(pixel), '\n'])
+f.close()
+
