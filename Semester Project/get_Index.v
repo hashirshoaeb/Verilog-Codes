e4b7acdb529f013dbadf3de2470aa0c5256d32da
hashirshoaeb <hashirshoaeb@gmail.com>
2019-12-09 00:44:42 +0500

    few more changings

--- Semester Project/get_Index.v
+++ Semester Project/get_Index.v
@@ -1,9 +1,21 @@
-module get_Index();
-    input i, j;
-    output l0,l1,l2,l3,l4,l5,l6;
-
-    always@()
+module get_Index(clk,i,j,l0,l1,l2,l3,l4,l5,l6,l7,l8);
+    input clk;
+    input [5:0] i, j;
+    output reg [11:0] l0,l1,l2,l3,l4,l5,l6,l7,l8;
+    parameter n = 64;
+    reg k;
+    always@(posedge clk)
     begin
-        
+        k = 0 + (i * n);
+        l0 = k - n + j - 1;
+        l1 = k - n + j;
+        l2 = k - n + (j + 1);
+        l3 = k + (j - 1);
+        l4 = k + j;
+        l5 = k + (j + 1);
+        l6 = k + n + (j - 1);
+        l7 = k + n + j;
+        l8 = k + n + (j + 1);
+        $monitor("%0d", l0, l1, l2, l3, l4, l5, l6, l7,l8);
     end
 endmodule
--- Semester Project/sobel.v
+++ Semester Project/sobel.v
@@ -22,22 +22,21 @@
 // $fwrite(id, out);
 // $fwrite(id, “\n”); 	//for newline
 // end
-
+`include "get_Index.v"
 module sobel();
     // https://timetoexplore.net/blog/initialize-memory-in-verilog
     reg [7:0] ROM_IN  [0:4095];  // = > 64*64 - 1 = > sizeofimage - 1
     reg [7:0] ROM_OUT [0:4095];
     reg clk;
-    integer n = 64;
-    integer i;
-    integer j;
+    reg [5:0] i, j;
+
+    wire [11:0] l0,l1,l2,l3,l4,l5,l6,l7,l8;
 
     initial
     begin
         $dumpfile("sobel.vcd");
         $dumpvars(0, sobel);
         clk = 0;
-        n = 64;
         i = 1;
         j = 1;
         #5
@@ -47,7 +46,7 @@ module sobel();
         $monitor("data_file handle was %0b", ROM_IN[38]);
         // writing to out.txt from ROM_IN from address 0 to 4095
         $writememb("out.txt",ROM_IN);
-        $finish;
+        
     end
     // ________________
     // | P0 | P1 | P2 |     --------> n columns
@@ -71,26 +70,24 @@ module sobel();
     // Variable ‘i’ represents row number whereas j represents column number
     // Variable ‘B’ is the base address of the array
     // Variable ‘n’ is the total number of the column of an image
-    output reg [13:0] k, l0, l1, l2, l3, l4, l5, l6, l7,l8;
+
+    get_Index obj(clk,i,j,l0,l1,l2,l3,l4,l5,l6,l7,l8);
+
     always@(posedge clk)
     begin  
-        for (i = 1; i <= n-2; i = i+1)
+        j = j+1;
+        if (j > 64-2)
         begin
-            k = 0 + (i * n);
-            for (j = 1; j <= n-2; j = j+1)
-            begin
-                #15
-                l0 = k - n + j - 1;
-                l1 = k - n + j;
-                l2 = k - n + (j + 1);
-                l3 = k + (j - 1);
-                l4 = k + j;
-                l5 = k + (j + 1);
-                l6 = k + n + (j - 1);
-                l7 = k + n + j;
-                l8 = k + n + (j + 1);
-                $monitor("%0d", l0, l1, l2, l3, l4, l5, l6, l7,l8);
-            end
-        end
+            j = 0;
+            i = i + 1;
+            if (i > 64-2)
+                i = 0;
+        end            
+    end
+
+    always // to generate clock
+    begin
+    #10
+    clk = !clk;
     end
 endmodule
